4ead87c3b8f6245a4db25aa6c303ed7dc07bb14b
Vita Gold <golvi1124@gmail.com>
2024-11-19 13:02:50 +0100

    0.opp

---
+++ script.js
@@ -0,0 +1,13 @@
+  let person = {
+    fornavn: "Vita",
+    etternavn: "Goldmane",
+    alder: 34,
+    hobbyer: ["lesing", "strikking", "hekling", "lek med katter"],
+    byen: "Bergen", 
+};
+
+console.log(person);
+
+console.log(person.fornavn);
+
+console.log(person.hobbyer[0])
